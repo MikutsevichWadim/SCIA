409acaffe33d1ed9d517d0b5185a4fab3db92f4b
MikutsevichWadim <123213069+MikutsevichWadim@users.noreply.github.com>
2024-06-27 16:47:24 +0300

    s

--- lw/13-14/script.js
+++ lw/13-14/script.js
@@ -1,60 +1,97 @@
-$(document).ready(function(){
+
+document.querySelector('#bGetUrl').addEventListener("click", function() {
+    console.log('lol')
+    document.getElementById('url').innerHTML = window.location.href
+})
 
 // задание 2
 
-    $("#bGetUrl").click(function(){
-        $("#url").innerHTML = location.href
-    })
+secondElement = document.body.children[1]
 
-    $("#deleteThisImage").click(function(){
-        $("#deleteThisImage").remove()
-    })
+secondElement.addEventListener("click", function() {
+    secondElement.remove()
+})
 
-// задание 3
+secondElement.addEventListener("mouseover", function(){
+    with (secondElement.style) {
+        color = "blue"
+        fontSize = "25px"
+        border = "1px solid green"
+    }
+    secondElement.classList.add("classOnHover")
+})
 
-    $("#favoriteSite").hover(
-        function(){
-            $("#favoriteSite").addClass("classOnHover")
-        },
-        function(){
-            $("#favoriteSite").removeClass("classOnHover")
-        }
-    )
+secondElement.addEventListener("mouseout", function(){
+    with (secondElement.style) {
+        color = "inherit"
+        fontSize = "inherit"
+        border = "none"
+    }
+    secondElement.classList.remove("classOnHover")
+})
 
 // задание 4
 
-    $("#getPCName").click(function(){
-        const os = require("os");
-        const hostname = os.hostname();
-        $("#PCName").innerHTML = window.location.hostname;
-        window.open("http://google.com", "_blank");
-    })
-
+document.querySelector("#bGetPCName").addEventListener("click", function() {
+    var features = "width=300,height=500,scrollbars=yes,status=no";
+    var newWindow = window.open("", "newWindow", features);
+    newWindow.document.write("<html><head><title>New Window</title></head><body>");
+    newWindow.document.write("<h1>Привет, мир!</h1>");
+    newWindow.document.write("<p>Это новое окно размером 300x500 пикселей.</p>");
+    newWindow.document.write("</body></html>");
 })
 
 // задание 5
+
+const emailField = document.getElementById('email');
+const maxLengthField = document.getElementById('max-length-field');
     
-document.getElementById('education-job-form').addEventListener('submit', function(event) {
+document.getElementById('pusk').addEventListener('click', function(event) {
+
     // Получение всех обязательных полей формы
-    var requiredFields = document.querySelectorAll('#education-job-form [required]');
 
     var isValid = true;
 
-    requiredFields.forEach(function(field) {
-        if (!field.value) {
-            isValid = false;
-            field.style.border = '2px solid red'; // Устанавливаем красную границу для незаполненного поля
-        } else {
-            field.style.border = ''; // Убираем границу, если поле заполнено
-        }
-    });
+    const emailValue = emailField.value;
+
+    // Проверка на валидность email с использованием регулярного выражения
+    if (isValidEmail(emailValue)) {
+        // Устанавливаем стили для корректного ввода
+        emailField.style.borderColor = 'green';
+    } else {
+        // Устанавливаем стили для некорректного ввода
+        emailField.style.borderColor = 'red';
+        isValid = false;
+    }
+
+    const maxLengthFieldValue = maxLengthField.value
+
+    if (isValidLength(maxLengthFieldValue)) {
+        maxLengthField.style.borderColor = 'green';
+    }
+    else {
+        maxLengthField.style.borderColor = 'red';
+        isValid = false;
+    }
 
     // Если форма невалидна, предотвращаем её отправку
     if (!isValid) {
-        alert('Пожалуйста, заполните все обязательные поля.');
-        event.preventDefault();
     }
     else {
         alert("Форма отправлена");
     }
 });
+
+function isValidEmail(email) {
+    // Регулярное выражение для проверки валидности email
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function isValidLength(maxLengthFieldValue) {
+    // Регулярное выражение для проверки валидности email
+    if (maxLengthFieldValue.length > 20) {
+        return false
+    }
+    return true
+}
